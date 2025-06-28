10a3b7a11e0a53cd0d40092f1df8f0f1c7094944
medet-naukhan <medet9802@icloud.com>
2025-06-29 01:24:26 +0500

    refactoring, connected sql, docker

--- cinema_v1/cmd/main.go
+++ cinema_v1/cmd/main.go
@@ -4,14 +4,50 @@ import (
 	"cinema_v1/internal/handlers"
 	"cinema_v1/internal/repository"
 	"cinema_v1/internal/service"
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	dbHost := "db"
+	dbPort := "5432"
+	dbUser := "user"
+	dbPassword := "password"
+	dbName := "moviesdb"
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		logrus.Fatalf("Failed to open database connection: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		logrus.Fatalf("Failed to connect to database: %v", err)
+	}
+	logrus.Info("Successfully connected to PostgreSQL database!")
+
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			logrus.Errorf("Error closing database connection: %v", err)
+		}
+		logrus.Info("Database connection closed.")
+	}()
+
 	// Инициализация хранилища в памяти
-	repo := repository.NewMovieMemoryRepository()
+	repo := repository.NewMoviePostgresRepository(db)
+
+	err = repo.(*repository.MoviePostgresRepository).EnsureTableExists()
+	if err != nil {
+		logrus.Fatalf("Failed to ensure database table exists: %v", err)
+	}
+	logrus.Info("Database table 'movies' checked/created successfully.")
 
 	// Инициализация сервиса
 	service := service.NewMovieService(repo)
